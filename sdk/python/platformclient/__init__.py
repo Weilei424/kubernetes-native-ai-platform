17bdba654e8d74c6db44b25167ea6378bcd91279
Mason Wang <mason123wp@gmail.com>
2026-04-13 00:11:01 -0400

    feat: expose PlatformClient and spec dataclasses as top-level SDK exports

---
+++ sdk/python/platformclient/__init__.py
@@ -0,0 +1,4 @@
+from .client import PlatformClient
+from .specs import DeploymentSpec, ResourceSpec, RuntimeSpec, TrainingSpec
+
+__all__ = ["PlatformClient", "TrainingSpec", "RuntimeSpec", "ResourceSpec", "DeploymentSpec"]
